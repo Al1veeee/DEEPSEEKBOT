f548ee120c2b299b1cb26cc0337ea849c9d17561
Al1veeee <al1veeee@users.noreply.github.com>
2026-01-19 12:12:34 +0300

    New items

--- generate.py
+++ generate.py
@@ -6,7 +6,6 @@ sys.stdout.reconfigure(encoding="utf-8")
 # Базовая директория проекта (где лежит generate.py и prompt.txt)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROMPT_PATH = os.path.join(BASE_DIR, "prompt.txt")
-
 # Импортируем функцию генерации из yandex.py вместо OpenAI
 from yandex import ai_generate, BASE_PROMPT
 
